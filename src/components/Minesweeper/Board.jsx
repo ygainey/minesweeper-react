0bd5de50b6a34d2359aae06a11be54b227e8d10b
YGainey <yousef.gainey@gmail.com>
2025-02-08 16:53:20 +0000

    correction

--- src/components/Minesweeper/Board.jsx
+++ src/components/Minesweeper/Board.jsx
@@ -1,6 +1,5 @@
 import Cell from './Cell'
 
-
 const Board = ({ board, onCellClick, onCellRightClick, gameOver, explodedMineIndex }) => (
     <div className="grid grid-cols-16 gap-0 bg-gray-400 p-2 rounded w-max">
         {board.map((cell) => (
